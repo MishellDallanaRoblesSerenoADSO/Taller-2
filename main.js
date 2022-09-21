926a7b8e0183ac0ce6e97a013506ba0cee897521
MishellDallanaRoblesSerenoADSO <mishellrobles2004@gmail.com>
2022-09-21 09:46:44 -0500

    Ejercicio 1

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 addEventListener("DOMContentLoaded",()=>{
     // 1. Escribir un programa que sume, reste, multiplique y divida dos números
 
@@ -8,3 +9,35 @@ addEventListener("DOMContentLoaded",()=>{
     console.log(`la multiplicacion es ${num1} * ${num2} = ${num1 * num2}`);
     console.log(`la division es ${num1} / ${num2} = ${num1 / num2}`);
 })
+=======
+<<<<<<< HEAD
+addEventListener("DOMContentLoaded", (e)=>{
+    //30. Ingrese una frase que lo identifique como programador de Software y luego mostar esta frase
+    // invertida
+
+    // frase
+    let f= prompt("Coloque la frase: ")
+
+    console.log(`${f}`)
+
+    let finvertido =f.split("").reverse().join("")
+    console.log(`${finvertido}`)
+})
+=======
+addEventListener("DOMContentLoaded",()=>{
+    //21. Se requiere calcular la distancia entre dos puntos, realizar el código que permita hallar la solución,
+    // por favor utilizar funciones matemáticas de Python para ello
+    // d = rc((x2-x1)^2 + (y2-y1)^2)
+
+    // Valores de los puntos
+    let x1=Number(prompt("Coloca la x1: ",7))
+    let x2=Number(prompt("  Coloca la ordenada 1: ",8))
+    let y1=Number(prompt("Coloca la y1: ",4))
+    let y2=Number(prompt("  Coloca la ordenada 2: ",6))
+    
+    // operación
+    let r=Number((x2-x1)**2+(y2-y1)**2)
+    console.log(`La distancia de los puntos es de: ${r}`)
+})
+>>>>>>> 3b61eaa (Ejercicio 21)
+>>>>>>> 8205e08 (Ejercicio 1)
